ffc32af5c9715fe53637f06596df3c3481adfabd
Moola Harshitha <harshithamoola99@gmail.com>
2025-08-19 21:40:24 +0530

    Add jumphost VM for stretch goal

---
+++ jumphost.tf
@@ -0,0 +1,36 @@
+resource "azurerm_network_interface" "jumphost_nic" {
+  name                = "jumphost-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sn_jumphost.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "jumphost" {
+  name                  = "jumphost-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.jumphost_nic.id]
+  size                  = "Standard_B1s"
+
+  admin_username        = "azureuser"
+  admin_password        = "Password1234!"  # Sample password; for real projects, use secrets!
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "Windows-10"
+    sku       = "win10-21h2-pro"
+    version   = "latest"
+  }
+
+  tags = local.common_tags
+}
